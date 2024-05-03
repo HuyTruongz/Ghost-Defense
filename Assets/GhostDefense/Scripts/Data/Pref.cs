4d23919a0566c4f4d9f5451832d9eeec2fadad21
Huy Trương <huytruongth33@gmail.com>
2024-05-03 17:44:37 +0700

    Create Pref script
    Create Pref script

---
+++ Assets/GhostDefense/Scripts/Data/Pref.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace UHUY.GhostDefense
+{
+    public static class Pref
+    {
+        public static bool IsFirstTime
+        {
+            set => SetBool(KeyPref.IsFirstTime.ToString(), value);
+            get => GetBool(KeyPref.IsFirstTime.ToString(), true);
+        }
+
+        public static int SpriteOrder
+        {
+            set => PlayerPrefs.SetInt(KeyPref.SpriteOrder.ToString(), value);
+            get => PlayerPrefs.GetInt(KeyPref.SpriteOrder.ToString(), 0);
+        }
+
+        public static string GameData
+        {
+            set => PlayerPrefs.SetString(KeyPref.GameData.ToString(), value);
+            get => PlayerPrefs.GetString(KeyPref.GameData.ToString(), "");
+        }
+
+        public static void SetBool(string key, bool v)
+        {
+            if (v)
+            {
+                PlayerPrefs.SetInt(key, 1);
+            }
+            else
+            {
+                PlayerPrefs.SetInt(key, 0);
+            }
+        }
+
+        public static bool GetBool(string key, bool defaultValue)
+        {
+            if (PlayerPrefs.HasKey(key))
+            {
+                int valFromPlayer = PlayerPrefs.GetInt(key);
+                if (valFromPlayer == 1)
+                {
+                    return true;
+                }
+                else if (valFromPlayer == 0)
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                return defaultValue;
+            }
+
+            return defaultValue;
+
+        }
+    }
+}
