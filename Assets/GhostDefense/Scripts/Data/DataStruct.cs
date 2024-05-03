93b4b0063f4f4054c7a44c96a89838d34b6c7dff
Huy Trương <huytruongth33@gmail.com>
2024-05-03 17:12:05 +0700

    Create DataStruct Script
    Create DataStruct Script

---
+++ Assets/GhostDefense/Scripts/Data/DataStruct.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum GameTag
+{
+    Player,
+    Enemy
+}
+
+public enum GameScene
+{
+    Gameplay,
+    MaiMenu
+}
+
+public enum KeyPref
+{
+    GameData,
+    IsFirstTime,
+    SpriteOrder
+}
+
+public enum Direction
+{
+    Left, Right, Top, Bottom, None
+}
+
+public enum GameState
+{
+    Starting,
+    Playing,
+    Wining,
+    Gameover
+}
+
+public enum PlayerSate
+{
+    Idel,
+    Walk,
+    Run,
+    Dead,
+    Attack,
+    Ultimate,
+    Dash
+}
+
+public enum AIState
+{
+    Walk,
+    Dash1,
+    Dash2,
+    Attack,
+    GotHit,
+    Ultimate,
+    Dead
+}
+
+public enum PlayerCollider
+{
+    Normal,
+    Dead
+}
